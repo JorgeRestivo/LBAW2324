efd5940dd8629bdd1ac1b96315ed7ef0805c4109
Letícia Coelho <leticiamendes2122@gmail.com>
2023-11-22 20:28:22 +0000

    better now

--- template-laravel/resources/views/begin.blade.php
+++ template-laravel/resources/views/begin.blade.php
@@ -1,20 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="event-container">
-            @foreach ($events as $event)
-                <div class="event-box">
-                    <div class="event-photo">
-                        <img src="{{ asset('photos/' . $event->photo) }}" alt="Event Photo">
-                    </div>
-                    <div class="event-details">
-                        <h2>{{ $event->eventname }}</h2>
-                        <p>Start Date: {{ $event->startdatetime }}</p>
-                        <p>End Date: {{ $event->enddatetime }}</p>
-                    </div>
+    <div class="event-container">
+        @foreach ($events as $event)
+            <div class="event-box">
+                <div class="event-photo">
+                    <img src="{{ asset('photos/' . $event->photo) }}" alt="Event Photo">
                 </div>
-            @endforeach
-        </div>
+                <div class="event-details">
+                    <h2>{{ $event->eventname }}</h2>
+                    <p>Start Date: {{ $event->startdatetime }}</p>
+                    <p>End Date: {{ $event->enddatetime }}</p>
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
