b60b1c0a5f6f6368dc2cb44b514f4a1c400b7e8e
Letícia Coelho <leticiamendes2122@gmail.com>
2023-11-23 18:45:57 +0000

    my events details

--- template-laravel/resources/views/myevents.blade.php
+++ template-laravel/resources/views/myevents.blade.php
@@ -7,8 +7,18 @@
         @if(count($myEvents) > 0)
             <ul>
                 @foreach($myEvents as $event)
-                    <li>{{ $event->eventname }} - {{ $event->startdatetime }}</li>
-                    <!-- Add more details as needed -->
+                <div class="event-box">
+                <a href="{{ route('event.show', ['id' => $event->id]) }}">
+                    <div class="event-photo">
+                        <img src="{{ asset('photos/' . $event->photo) }}" alt="Event Photo">
+                    </div>
+                    <div class="event-details">
+                        <h2>{{ $event->eventname }}</h2>
+                        <p>Start Date: {{ $event->startdatetime }}</p>
+                        <p>End Date: {{ $event->enddatetime }}</p>
+                    </div>
+                </a>
+            </div>
                 @endforeach
             </ul>
         @else
