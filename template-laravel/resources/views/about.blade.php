5d70899737e6f0fccb126745358faef43e2d05c9
Lara InAas Alves Cunha <up202108876@fe.up.pt>
2023-12-04 17:43:06 +0000

    Update about.blade.php

--- template-laravel/resources/views/about.blade.php
+++ template-laravel/resources/views/about.blade.php
@@ -1,13 +1,7 @@
 <!-- Dentro do arquivo resources/views/about.blade.php -->
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>About - {{ config('app.name', 'Laravel') }}</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>About</h1>
     <p>
         'GetTogether' is an innovative event management website that aims to make it easier to create, organize, and participate in events of all types. Our vision is to provide an intuitive and comprehensive platform that connects people and makes the experience of planning and participating in more collaborative and memorable events.
@@ -18,5 +12,4 @@
         <li>Group member 2: Letícia Coelho, <a href="mailto:up202108877@edu.fe.up.pt">up202108877@edu.fe.up.pt</a></li>
         <li>Group member 3: Lara Cunha, <a href="mailto:up202108876@edu.fe.up.pt">up202108876@edu.fe.up.pt</a></li>
     </ul>
-</body>
-</html>
+@endsection
