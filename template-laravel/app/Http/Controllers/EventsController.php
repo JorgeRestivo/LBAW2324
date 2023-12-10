a46454ad65d5b3b56cc502dfb0cec43bffd8b2ac
Letícia Coelho <leticiamendes2122@gmail.com>
2023-12-10 18:49:12 +0000

    corrected tags

--- template-laravel/app/Http/Controllers/EventsController.php
+++ template-laravel/app/Http/Controllers/EventsController.php
@@ -42,7 +42,7 @@ class EventsController extends Controller
         'id',
         function ($query) use ($userId) {
             $query->select('event_id')
-                ->from('eventinvitation') // Adjust the case here
+                ->from('eventinvitation')
                 ->where('user_invited_id', $userId);
         }
     )->where('ispublic', false)->get()->toArray();
@@ -335,18 +335,49 @@ public function removeAttendee($eventId, $userId)
         $tags = Tag::all();
         $tagId = $request->query('tag');
 
+        $userId = auth()->id();
+
         if ($tagId) {
-            // Retrieve events associated with the specified tag
-            $events = Event::where('tag_id', $tagId)->get();
+            // Retrieve public events associated with the specified tag
+            $publicEvents = Event::where('tag_id', $tagId)
+                ->where('ispublic', true)
+                ->get();
+
+            // Retrieve private events for which the user was invited associated with the specified tag
+            $privateEvents = Event::whereIn(
+                'id',
+                function ($query) use ($userId, $tagId) {
+                    $query->select('events.id')
+                        ->from('events')
+                        ->join('eventinvitation', 'events.id', '=', 'eventinvitation.event_id')
+                        ->where('eventinvitation.user_invited_id', $userId)
+                        ->where('events.ispublic', false)
+                        ->where('events.tag_id', $tagId);
+                }
+            )->get();
+
+            $events = $publicEvents->merge($privateEvents);
         } else {
-            // If no tag is specified, retrieve all events
-            $events = Event::all();
+            // If no tag is specified, retrieve all public events and private events for which the user was invited
+            $publicEvents = Event::where('ispublic', true)->get();
+
+            $privateEvents = Event::whereIn(
+                'id',
+                function ($query) use ($userId) {
+                    $query->select('event_id')
+                        ->from('eventinvitation')
+                        ->where('user_invited_id', $userId);
+                }
+            )->where('ispublic', false)->get();
+
+            $events = $publicEvents->merge($privateEvents);
         }
 
         // ... pass $events and other necessary data to the view ...
 
         return view('events.filtered', compact('events'), ['tags' => $tags]);
     }
+
 
 }
 
